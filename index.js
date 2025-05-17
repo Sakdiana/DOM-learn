f6745960ef42db3f85b7ca87ecdafa21f625e5e1
Sakdiana <bekmurzaevasakdiana2@gmail.com>
2025-05-17 10:49:18 +0600

    task FirstLesson

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-let table=document.body.firstElementChild
-for(let i=0;i<table.rows.length;i++){
-    let row=table.rows[i]
-    row.cells[i].style.backgroundColor='red'
+let table = document.body.firstElementChild;
+
+// Красим главную диагональ
+for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    row.cells[i].style.backgroundColor = 'red';
+}
+
+// Красим обратную диагональ
+for (let i = 0; i < table.rows.length; i++) {
+    let row = table.rows[i];
+    row.cells[row.cells.length - 1 - i].style.backgroundColor = 'blue';
+}
+
+// Красим нижнюю строку
+let lastRow = table.rows[table.rows.length - 1];
+for (let i = 0; i < lastRow.cells.length; i++) {
+    lastRow.cells[i].style.backgroundColor = 'green';
 }
