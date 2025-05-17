8888ba1d096056b8ddd19fd66c2142d2954fcdb7
Sakdiana <bekmurzaevasakdiana2@gmail.com>
2025-05-17 10:41:23 +0600

    firstLesson

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-alert('Привет детка горяченбкая')
+let table=document.body.firstElementChild
+for(let i=0;i<table.rows.length;i++){
+    let row=table.rows[i]
+    row.cells[i].style.backgroundColor='red'
+}
