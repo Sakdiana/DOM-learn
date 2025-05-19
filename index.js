b7999f95f17328de61f51085e6c196c56481b55b
Sakdiana <bekmurzaevasakdiana2@gmail.com>
2025-05-19 14:53:26 +0600

    secondLesson

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
-let table = document.body.firstElementChild;
+// let table = document.body.firstElementChild;
 
-// Красим главную диагональ
-for (let i = 0; i < table.rows.length; i++) {
-    let row = table.rows[i];
-    row.cells[i].style.backgroundColor = 'red';
-}
+// // Красим главную диагональ
+// for (let i = 0; i < table.rows.length; i++) {
+//     let row = table.rows[i];
+//     row.cells[i].style.backgroundColor = 'red';
+// }
 
-// Красим обратную диагональ
-for (let i = 0; i < table.rows.length; i++) {
-    let row = table.rows[i];
-    row.cells[row.cells.length - 1 - i].style.backgroundColor = 'blue';
-}
+// // Красим обратную диагональ
+// for (let i = 0; i < table.rows.length; i++) {
+//     let row = table.rows[i];
+//     row.cells[row.cells.length - 1 - i].style.backgroundColor = 'blue';
+// }
 
-// Красим нижнюю строку
-let lastRow = table.rows[table.rows.length - 1];
-for (let i = 0; i < lastRow.cells.length; i++) {
-    lastRow.cells[i].style.backgroundColor = 'green';
-}
+// // Красим нижнюю строку
+// let lastRow = table.rows[table.rows.length - 1];
+// for (let i = 0; i < lastRow.cells.length; i++) {
+//     lastRow.cells[i].style.backgroundColor = 'green';
+// }
+
+
+let a=document.getElementById("smth")
+a.style.color="red"
+
+let b=document.querySelector(".qwerty")
+b.style.color="green"
+
+let c=document.querySelectorAll("li")
+console.log(c);
